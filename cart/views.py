4099332477a4f7484483f98aa8401e6343c36b87
X00219608 <X00219608@mytudublin.ie>
2024-12-02 00:43:11 +0000

    stripe

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import redirect, render , get_object_or_404
 from shop.models import Coffee
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
+import stripe
+from django.conf import settings
+from django.urls import reverse
 
 def _cart_id(request):
     cart = request.session.session_key
@@ -25,7 +28,7 @@ def add_cart(request, coffee_id):
         cart_item = CartItem.objects.create(coffee=coffee, quantity=1,cart=cart)
     return redirect('cart:cart_detail')
 
-def cart_detail(request, total=0, counter=0, cart_items = None):
+def cart_detail(request, total=0, counter=0, cart_items=None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, active=True)
@@ -34,12 +37,46 @@ def cart_detail(request, total=0, counter=0, cart_items = None):
             counter += cart_item.quantity
     except ObjectDoesNotExist:
         pass
-    return render(request, 'cart.html',
-                    {'cart_items':cart_items,
-                    'total':total,
-                    'counter':counter
-                    })
 
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    stripe_total = int(total * 100)
+    description = 'Online Shop - New Order'
+
+    if request.method == 'POST':
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                payment_method_types=['card'],
+                line_items=[{
+                    'price_data': {
+                        'currency': 'eur',
+                        'product_data': {
+                            'name': 'Order from Perfect Cushion Shop',
+                        },
+                        'unit_amount': stripe_total,
+                    },
+                    'quantity': 1,
+                }],
+                mode='payment',
+                billing_address_collection='required',
+                shipping_address_collection={},
+                payment_intent_data={'description': description},
+                success_url=request.build_absolute_uri(reverse('shop:home')),
+                cancel_url=request.build_absolute_uri(reverse('cart:cart_detail')),
+            )
+            return redirect(checkout_session.url, code=303)
+        except Exception as e:
+            return render(request, 'cart.html', {
+                'cart_items': cart_items,
+                'total': total,
+                'counter': counter,
+                'error': str(e),
+            })
+
+    return render(request, 'cart.html', {
+        'cart_items': cart_items,
+        'total': total,
+        'counter': counter,
+    })
 
 def cart_remove(request, coffee_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
@@ -51,8 +88,6 @@ def cart_remove(request, coffee_id):
     else:
         cart_item.delete()
     return redirect('cart:cart_detail')
-
-
 
 def full_remove(request, coffee_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
